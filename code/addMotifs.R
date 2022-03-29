82c39e90c19d1ae051efca06fbcf9cbdb2e7cde2
muhligs <muhligs@users.noreply.github.com>
2022-03-29 11:16:36 +0200

    removing typos, Thanks @Juan Trinidad!

--- code/addMotifs.R
+++ code/addMotifs.R
@@ -11,14 +11,14 @@ addMotifs <- function(sco,motifs=7){
   cat("adding motif models.\n")
   if(!is.numeric(motifs)|!length(motifs)==1) stop("Motifs needs to be a number representing a k-mer from 5 to 8")
     file <- paste0("./motif.models/patterns.",motifs,"mer.Rdata")
-    if(file.exists(file)) load(file) else stop(paste0("Please prepare motif models for ",moitfs,"-mers."))
+    if(file.exists(file)) load(file) else stop(paste0("Please prepare motif models for ",motifs,"-mers."))
     sco$motifModels <- patterns
     names(sco$motifModels) <- Regmex::all.mers(motifs)
   cat(length(sco$motifModels)," motif models added.\n\n")
   ################################# 
   # check if seqXmot calculations were already done (True for 5,6,7 mers)
     file <- paste0("./motif.models/",sco$runparameters$species,".seqXmot.",sco$runparameters$seq.type,"_mrs_",sco$runparameters$motifs,"mer.rds")
-    if(!file.exists(file)) stop(paste0("Please prepare sequence specific probabilities for ",moitfs,"-mers in species ",sco$runparameters$species,"."))
+    if(!file.exists(file)) stop(paste0("Please prepare sequence specific probabilities for ",motifs,"-mers in species ",sco$runparameters$species,"."))
       cat("Sequence specific probabilities were pre-calculated. Loading...\n")
       pval.mat <- readRDS(file)
       # load("./motif.models/utr3.seqs.Rdata") # load match file (not needed because colnames are tid per construction)
@@ -30,7 +30,7 @@ addMotifs <- function(sco,motifs=7){
     ################################# # in case motifs are standard k-mers, re-use if possible (5,6,7mers)
       if(is.numeric(motifs))ksize <- motifs
       file <- paste0("./motif.models/",sco$runparameters$species,".seqXmot.counts.",sco$runparameters$seq.type,"_mrs_",sco$runparameters$motifs,"mer.rds")
-      if(!file.exists(file)) stop(paste0("Please prepare motif counts for ",moitfs,"-mers in species ",sco$runparameters$species,"."))
+      if(!file.exists(file)) stop(paste0("Please prepare motif counts for ",motifs,"-mers in species ",sco$runparameters$species,"."))
       cat("Premade. Loading...\n")
       counts <- readRDS(file)
       sco$motifCounts <- counts[,sco$seqs$tid]
